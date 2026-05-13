68279cd95be6f0d7d2cc4db4dca55bd8462bccf1
qtthgdfg <idonchoose@gmail.com>
2026-05-13 10:22:36 +0000

    Clear MinerApplication class implementation
    Removed all code from MinerApplication class.

--- android/app/src/main/Java/com/monerominer/MinerApplication.kt
+++ android/app/src/main/Java/com/monerominer/MinerApplication.kt
@@ -1,22 +1,7 @@
 package com.monerominer
 
 import android.app.Application
-import android.util.Log
 
 class MinerApplication : Application() {
-    
-    override fun onCreate() {
-        super.onCreate()
-        
-        // Initialize crash logger
-        try {
-            CrashLogger.init(this)
-            CrashLogger.log("App started")
-        } catch (e: Exception) {
-            Log.e("MinerApp", "CrashLogger failed: ${e.message}")
-        }
-        
-        // DO NOT load library here
-        // Library will be loaded by MinerService when mining starts
-    }
+    // TOTALLY EMPTY - nothing at all
 }
